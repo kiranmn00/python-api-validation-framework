8f692747b702aecc363732e13cd1905a7ff5f4eb
kiranmn00 <149135554+kiranmn00@users.noreply.github.com>
2026-06-21 20:40:21 +0530

    Update validator.py

--- validator.py
+++ validator.py
@@ -1,13 +1,42 @@
 def validate_response(data):
 
-    checks = []
+```
+checks = []
 
-    checks.append(
-        ("Name Present", "name" in data)
+checks.append(
+    (
+        "Name Field Present",
+        "name" in data
     )
+)
 
-    checks.append(
-        ("Email Present", "email" in data)
+checks.append(
+    (
+        "Email Field Present",
+        "email" in data
     )
+)
 
-    return checks
+checks.append(
+    (
+        "Username Field Present",
+        "username" in data
+    )
+)
+
+checks.append(
+    (
+        "Address Present",
+        "address" in data
+    )
+)
+
+checks.append(
+    (
+        "Phone Present",
+        "phone" in data
+    )
+)
+
+return checks
+```
